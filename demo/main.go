148ad5e18217a92522cf3a4d573b5b1a289ecd64
Jianbo Feng <fengjianbo@nibirutech.com>
2019-06-10 20:54:09 +0800

    增加并发支持

--- demo/main.go
+++ demo/main.go
@@ -4,12 +4,16 @@ import (
 	"log"
 	"net/url"
 	"os"
+	"sync"
 	"time"
 
 	"cgv"
 	"github.com/athurg/wechat_work_sdk"
 	"github.com/tencentyun/scf-go-lib/cloudfunction"
 )
+
+var wechatClient *wechat.AgentClient
+var screenDay = time.Now().Format("2006-01-02")
 
 func main() {
 	if _, ok := os.LookupEnv("TENCENTCLOUD_RUNENV"); ok {
@@ -21,7 +25,7 @@ func main() {
 }
 
 func fetchAndNotify() {
-	wechatClient := wechat.NewAgentClientFromEnv()
+	wechatClient = wechat.NewAgentClientFromEnv()
 
 	cinema, err := cgv.CinemaByName("成都高新")
 	if err != nil {
@@ -30,55 +34,65 @@ func fetchAndNotify() {
 
 	log.Printf("影院信息%+v", cinema)
 
-	screenDay := time.Now().Format("2006-01-02")
 
 	//获取所有影片信息
 	movies, err := cgv.MoviesByThat(cinema.THAT_CD)
+
+	var wg sync.WaitGroup
 	for _, movie := range movies {
-		movie.MOV_NM, _ = url.QueryUnescape(movie.MOV_NM)
+		wg.Add(1)
+		go func(cinemaCode string, m cgv.Movie) {
+			defer wg.Done()
+			checkMovie(cinemaCode, m)
+		}(cinema.THAT_CD, movie)
+	}
+	wg.Wait()
+}
 
-		picUrl := "http://www.cgv.com.cn" + movie.FILE_PATH
-		detailUrl := "http://www.cgv.com.cn/movieDetail/gotoMovieDetail.fo?MOV_CD=" + movie.MOV_CD
+func checkMovie(cinemaCode string, movie cgv.Movie) {
+	movie.MOV_NM, _ = url.QueryUnescape(movie.MOV_NM)
 
-		//获取影片排片日期信息，以确保是否当日正在上映
-		dateList, err := cgv.ScheduleDateByCinemaMovie(cinema.THAT_CD, movie.MOV_CD)
-		if err != nil {
-			log.Println("\t", err)
-			continue
-		}
+	picUrl := "http://www.cgv.com.cn" + movie.FILE_PATH
+	detailUrl := "http://www.cgv.com.cn/movieDetail/gotoMovieDetail.fo?MOV_CD=" + movie.MOV_CD
 
-		var isTodayShow bool
-		for _, date := range dateList {
-			if date.SCN_DY == screenDay {
-				isTodayShow = true
-				break
-			}
-		}
+	//获取影片排片日期信息，以确保是否当日正在上映
+	dateList, err := cgv.ScheduleDateByCinemaMovie(cinemaCode, movie.MOV_CD)
+	if err != nil {
+		log.Println("\t", err)
+		return
+	}
 
-		if !isTodayShow {
-			continue
-		}
-
-		message := wechat.NewNewsMessage()
-		message.Append(movie.MOV_NM, detailUrl, "", picUrl)
-
-		//获取当日排片表
-		schedules, err := cgv.ScheduleInfoToday(cinema.THAT_CD, movie.MOV_CD)
-		if err != nil {
-			message.Append(movie.MOV_NM, detailUrl, err.Error(), "")
+	var isTodayShow bool
+	for _, date := range dateList {
+		if date.SCN_DY == screenDay {
+			isTodayShow = true
 			break
 		}
+	}
 
-		for _, s := range schedules {
-			s.SCREEN_NM, _ = url.QueryUnescape(s.SCREEN_NM)
-			s.SCN_FR_TM, _ = url.QueryUnescape(s.SCN_FR_TM)
-			s.SCN_TO_TM, _ = url.QueryUnescape(s.SCN_TO_TM)
-			message.Append(s.SCN_FR_TM+"～"+s.SCN_TO_TM+"，"+s.SCREEN_NM, "", "", "")
-		}
+	if !isTodayShow {
+		return
+	}
 
-		_, err = wechatClient.SendBatchNewsMessageToUsers(message, "@all")
-		if err != nil {
-			log.Println("发送消息错误", err)
-		}
+	message := wechat.NewNewsMessage()
+	message.Append(movie.MOV_NM, detailUrl, "", picUrl)
+
+	//获取当日排片表
+	schedules, err := cgv.ScheduleInfoToday(cinemaCode, movie.MOV_CD)
+	if err != nil {
+		message.Append(movie.MOV_NM, detailUrl, err.Error(), "")
+		return
+	}
+
+	for _, s := range schedules {
+		s.SCREEN_NM, _ = url.QueryUnescape(s.SCREEN_NM)
+		s.SCN_FR_TM, _ = url.QueryUnescape(s.SCN_FR_TM)
+		s.SCN_TO_TM, _ = url.QueryUnescape(s.SCN_TO_TM)
+		message.Append(s.SCN_FR_TM+"～"+s.SCN_TO_TM+"，"+s.SCREEN_NM, "", "", "")
+	}
+
+	_, err = wechatClient.SendBatchNewsMessageToUsers(message, "@all")
+	if err != nil {
+		log.Println("发送消息错误", err)
 	}
 }
